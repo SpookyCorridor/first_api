e2f86aaf7d2609445769bf250f4bac7c69c34b76
Dylan <invision33@gmail.com>
2015-07-23 12:39:44 -0500

    added color hash

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ get '/' do
 end
 
 get '/api/awesome' do
-
+	{:red => '#D6503F',
+	 :orange => '#D68909',
+	 :yellow => '#F2F748',
+	 :green => '#35D659',
+	 :blue => '#45CAF1',
+	 :indigo => '#7582F6',
+	 :violet => '#E067D6'
+	}
 end 
 
